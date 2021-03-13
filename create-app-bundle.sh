c73fd6f22b21e3e3e887a31bd612ef5a0d748776
Lothar Haeger <lothar.haeger@is4it.de>
2021-03-13 14:04:18 +0100

    Update and cleanup master

--- create-app-bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf AutoRaise.app && \
-mkdir -p AutoRaise.app/Contents/MacOS && \
-mkdir AutoRaise.app/Contents/Resources && \
-cp AutoRaise AutoRaise.app/Contents/MacOS && \
-cp Info.plist AutoRaise.app/Contents && \
-cp AutoRaise.icns AutoRaise.app/Contents/Resources && \
-chmod 700 AutoRaise.app && echo "Successfully created AutoRaise.app"
